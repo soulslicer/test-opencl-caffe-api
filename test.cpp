9b5137a4074fef4b39048d1656258e88dea3a714
Raaj <ryaadhav@andrew.cmu.edu>
2018-02-12 16:56:50 -0500

    wip

--- test.cpp
+++ test.cpp
@@ -13,6 +13,8 @@ void f(){viennacl::ocl::context &ctx = viennacl::ocl::get_context(0);}
 
 #define MULTI_LINE_STRING(ARG) #ARG
 
+#include <random>
+
 namespace op
 {
     const char *resizeAndMergeKernel = MULTI_LINE_STRING(
@@ -30,14 +32,17 @@ class Net
 {
 public:
     std::unique_ptr<caffe::Net<float>> upCaffeNet;
-    bool log = false;
+    bool log = true;
     int gpuID = 0;
-    caffe::device* device;
     std::shared_ptr<caffe::Caffe> caffe;
-    caffe::Caffe Caffe;
+
+    std::random_device rd;
+    std::mt19937 mt;
+    std::uniform_real_distribution<double> dist;
 
     Net(){
-
+        mt = std::mt19937(rd());
+        dist = std::uniform_real_distribution<double>(224, 230);
     }
 
     void initNet(std::string protoPath, std::string modelPath, int gpuID)
@@ -46,11 +51,9 @@ public:
         //caffe::Caffe::set_mode(caffe::Caffe::GPU);
         //caffe::Caffe::SetDevice(gpuID);
         this->gpuID = gpuID;
-        device = Caffe.GetDevice(gpuID,0);
-        Caffe.set_mode(caffe::Caffe::GPU);
-        Caffe.SetDevice(gpuID);
-        Caffe.SelectDevice(device);
-        upCaffeNet.reset(new caffe::Net<float>{protoPath, caffe::TEST, device});
+        cout << "GPU: " << this->gpuID << endl;
+        caffe::Caffe::SelectDevice(this->gpuID, false);
+        upCaffeNet.reset(new caffe::Net<float>{protoPath, caffe::TEST, caffe::Caffe::GetDefaultDevice()});
         upCaffeNet->CopyTrainedLayersFrom(modelPath);
         if(log) cout << "Net Loaded " << gpuID << endl;
 
@@ -61,31 +64,42 @@ public:
     }
 
     void forwardNet(){
+        cv::Size newSize(int(dist(mt)),int(dist(mt)));
+        //cv::Size newSize(120,150);
+        cout << newSize << endl;
+
         // Load 1 image
         cv::Mat image, image2, image3;
         image = cv::imread(std::string(CMAKE_CURRENT_SOURCE_DIR) + "/image.jpg");
-        cv::resize(image, image2, cv::Size(224,244));
+        cv::resize(image, image2, newSize);
         image2.convertTo(image3, CV_32FC3);
         caffe::BlobProto blob_proto;
         blob_proto.set_channels(3);
-        blob_proto.set_height(224);
-        blob_proto.set_width(224);
+        blob_proto.set_height(newSize.width);
+        blob_proto.set_width(newSize.height);
         blob_proto.clear_data();
         for (int c = 0; c < 3; ++c) {
-            for (int h = 0; h < 224; ++h) {
-                for (int w = 0; w < 224; ++w) {
+            for (int h = 0; h < newSize.height; ++h) {
+                for (int w = 0; w < newSize.width; ++w) {
                     blob_proto.add_data(image3.at<cv::Vec3f>(h, w)[c]);
                 }
             }
         }
         blob_proto.set_num(1);
         caffe::Blob<float>* input_layer = upCaffeNet->input_blobs()[0];
-        input_layer->FromProto(blob_proto);
         if(log) cout << "Image Loaded" << gpuID << endl;
 
+        upCaffeNet->blobs()[0]->Reshape({1,3,newSize.width,newSize.height});
+        upCaffeNet->Reshape();
+        if(log) cout << "Reshape Done" << gpuID << endl;
+
+        input_layer->FromProto(blob_proto);
+        if(log) cout << "Caffe Loaded" << gpuID << endl;
+
         // Forward Pass
-        upCaffeNet->Forward();
+        upCaffeNet->Forward(0);
         if(log) cout << "Forward Done" << gpuID << endl;
+        cout << "Forward Done" << gpuID << endl;
 
         // Get my original image as a cl::Buffer
         const float* gpuData = input_layer->gpu_data();
@@ -104,8 +118,11 @@ public:
             if(log) cout << "*******ERROR: " << e.err() << gpuID << endl;
             return;
         }
-        op::CLManager::getInstance(gpuID)->getQueue().enqueueNDRangeKernel(resizeAndMergeKernel, cl::NDRange(), cl::NDRange(224,224), cl::NDRange(), NULL, NULL);
+        op::CLManager::getInstance(gpuID)->getQueue().enqueueNDRangeKernel(resizeAndMergeKernel, cl::NDRange(), cl::NDRange(newSize.width,newSize.height), cl::NDRange(), NULL, NULL);
         if(log) cout << "OpenCL Kernel Run" << gpuID << endl;
+
+        cout << dist(mt) << endl;
+
     }
 };
 
@@ -117,20 +134,22 @@ void thread_handler(int gpuID){
     Net n;
     //m.lock();
     n.initNet(protoPath, modelPath, gpuID);
-    for(int i=0; i<500; i++)
-    n.forwardNet();
+    for(int i=0; i<1000; i++){
+        n.forwardNet();
+    }
+    cout << "Finish" << endl;
     //m.unlock();
 }
 
 int main(){
-    caffe::Caffe::set_mode(caffe::Caffe::GPU);
-    caffe::Caffe::SetDevice(0);
+    caffe::Caffe::set_mode(caffe::Caffe::GPU);  // Global effect, ONLY to be used on main thread
+    // Select all absolute/global device IDs you want to use for computation later on
+    std::vector<int> device_ids = {0,1};  // You could also use GPUs 2 and 3, but you wouldn't have to change the IDs for below if you use `list_id = true`.
+    caffe::Caffe::SetDevices(device_ids);
 
     std::thread t1(&thread_handler,0);
-    std::thread t2(&thread_handler,1);
-    std::thread t3(&thread_handler,3);
+    //std::thread t2(&thread_handler,1);
     t1.join();
-    t2.join();
-    t3.join();
+    //t2.join();
     return 0;
 }
